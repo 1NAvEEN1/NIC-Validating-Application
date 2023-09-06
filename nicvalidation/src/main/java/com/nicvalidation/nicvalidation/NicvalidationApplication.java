5af45e552a7604a2c3d90455e1d14a4281b71dcf
Naveen Jayathilaka <tharindunaveen9@gmail.com>
2023-09-06 12:36:26 +0530

    poject initialized

---
+++ nicvalidation/src/main/java/com/nicvalidation/nicvalidation/NicvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.nicvalidation.nicvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicvalidationApplication.class, args);
+	}
+
+}
---
+++ nicvalidation/src/test/java/com/nicvalidation/nicvalidation/NicvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicvalidation.nicvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicvalidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
